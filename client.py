fcc33f0122c2118ad064e7b684725b12ee5cb6b6
domhall <dhall@worth.systems>
2020-08-21 17:15:36 +0100

    Test for linking stashshare to firebase

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
 import stashshare
+import requests
 
 def send_changes():
     patch = stashshare.save()
+    r = requests.post("https://us-central1-stash-share.cloudfunctions.net/sendPatch", json =
+    {
+        'name': 'blue mob',
+        'patch': patch
+    })
+    print(r.text)
 
 
 def receive_changes():
     patch = 'get patch here'
     stashshare.apply(patch)
+
+def test_endpoint():
+    r = requests.post("https://us-central1-stash-share.cloudfunctions.net/sendPatch", json =
+    {
+        'test': 'hi'
+    })
+    print(r.json()['test'])
+
+test_endpoint()
