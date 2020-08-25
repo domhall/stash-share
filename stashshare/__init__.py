f7973fbbae2201787f1bc248fcfd5a0d3ecafa0d
domhall <dhall@worth.systems>
2020-08-25 10:12:01 +0100

    Stopped using stash. apply now loses progress

--- stashshare/__init__.py
+++ stashshare/__init__.py
@@ -1,17 +1,12 @@
-# git stash show -p > patch
-
-# git apply patch
-# git stash
 import subprocess
 
 def save():
-    subprocess.Popen(['git', 'stash']).wait()
-    p = subprocess.Popen(['git', 'stash', 'show', '-p'],
+    p = subprocess.Popen(['git', 'diff'],
                      stdout=subprocess.PIPE)
     patch, _ = p.communicate()
-    subprocess.Popen(['git', 'stash', 'apply']).wait()
     return patch
 
 def apply(patch):
+    subprocess.Popen(['git', 'reset', '--hard'], stdin=subprocess.PIPE).wait()
     p = subprocess.Popen(['git', 'apply'], stdin=subprocess.PIPE)
     p.communicate(patch)
